0765dd775c6df8e57f89209860722d1f4474ab78
idiotcatalyst@gmail.com <idiotcatalyst@gmail.com@68ceebe8-dcee-985c-7ae1-c1b0541a3324>
2013-02-03 03:42:52 +0000

    Added initial, successfully building ARM Eclipse project

---
+++ trunk/code/system/main.c
@@ -0,0 +1,24 @@
+#define __MAIN_C__
+#include <stdint.h>
+
+#include "stm32f4xx.h"
+#include "global_defs.h"
+//#include "FreeRTOS.h"
+//#include "task.h"
+//#include "timers.h"
+//#include "semphr.h"
+
+int main(void)
+{
+   // Init stuff
+
+   while (TRUE)
+   {
+
+
+   }
+
+
+
+   return 0;
+}
