08b212dac6e48c3c48b767c9686a478d5494be81
idiotcatalyst <idiotcatalyst@gmail.com>
2013-02-09 19:28:05 +0000

    Fixed weird eclipse gcc options issue; changed to blink one LED in the main loop; not building completely yet

--- code/system/main.c
+++ code/system/main.c
@@ -8,7 +8,7 @@
 //#include "semphr.h"
 
 static GPIO_InitTypeDef PG6_INIT = { GPIO_Pin_6, GPIO_Mode_OUT, GPIO_Speed_50MHz, GPIO_OType_PP, GPIO_PuPd_UP };
-static GPIO_InitTypeDef PG8_INIT = { GPIO_Pin_8, GPIO_Mode_OUT, GPIO_Speed_50MHz, GPIO_OType_PP, GPIO_PuPd_UP };
+//static GPIO_InitTypeDef PG8_INIT = { GPIO_Pin_8, GPIO_Mode_OUT, GPIO_Speed_50MHz, GPIO_OType_PP, GPIO_PuPd_UP };
 
 int main(void)
 {
@@ -23,20 +23,22 @@ int main(void)
    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG, ENABLE);
 
    GPIO_Init(GPIOG, &PG6_INIT);
-   GPIO_Init(GPIOG, &PG8_INIT);
+   //GPIO_Init(GPIOG, &PG8_INIT);
 
    while (TRUE)
    {
       for (blink_counter = 0; blink_counter < 0x0001FFFF; blink_counter++)
       {
          // Turn on LED
-         GPIO_SetBits(GPIOG, GPIO_Pin_6 | GPIO_Pin_8);
+         //GPIO_SetBits(GPIOG, GPIO_Pin_6 | GPIO_Pin_8);
+         GPIO_SetBits(GPIOG, GPIO_Pin_6);
       }
 
       for (blink_counter = 0; blink_counter < 0x001FFFFF; blink_counter++)
       {
          // Turn off LED
-         GPIO_ResetBits(GPIOG, GPIO_Pin_6 | GPIO_Pin_8);
+         //GPIO_ResetBits(GPIOG, GPIO_Pin_6 | GPIO_Pin_8);
+         GPIO_ResetBits(GPIOG, GPIO_Pin_6);
       }
    }
 
