e6dd2ed2ce992bba7bdef6ab228ecada54e509ea
idiotcatalyst <idiotcatalyst@gmail.com>
2013-02-25 02:46:10 +0000

    Fixed a couple compiler warnings due to an unused local

--- code/system/lwIP/core/tcp_out.c
+++ code/system/lwIP/core/tcp_out.c
@@ -842,7 +842,9 @@ err_t
 tcp_send_empty_ack(struct tcp_pcb *pcb)
 {
   struct pbuf *p;
+#if CHECKSUM_GEN_TCP
   struct tcp_hdr *tcphdr;
+#endif
   u8_t optlen = 0;
 
 #if LWIP_TCP_TIMESTAMPS
@@ -856,7 +858,7 @@ tcp_send_empty_ack(struct tcp_pcb *pcb)
     LWIP_DEBUGF(TCP_OUTPUT_DEBUG, ("tcp_output: (ACK) could not allocate pbuf\n"));
     return ERR_BUF;
   }
-  tcphdr = (struct tcp_hdr *)p->payload;
+
   LWIP_DEBUGF(TCP_OUTPUT_DEBUG, 
               ("tcp_output: sending ACK for %"U32_F"\n", pcb->rcv_nxt));
   /* remove ACK flags from the PCB, as we send an empty ACK now */
@@ -872,6 +874,7 @@ tcp_send_empty_ack(struct tcp_pcb *pcb)
 #endif 
 
 #if CHECKSUM_GEN_TCP
+  tcphdr = (struct tcp_hdr *)p->payload;
   tcphdr->chksum = inet_chksum_pseudo(p, &(pcb->local_ip), &(pcb->remote_ip),
         IP_PROTO_TCP, p->tot_len);
 #endif
@@ -1364,7 +1367,9 @@ void
 tcp_keepalive(struct tcp_pcb *pcb)
 {
   struct pbuf *p;
+#if CHECKSUM_GEN_TCP
   struct tcp_hdr *tcphdr;
+#endif
 
   LWIP_DEBUGF(TCP_DEBUG, ("tcp_keepalive: sending KEEPALIVE probe to %"U16_F".%"U16_F".%"U16_F".%"U16_F"\n",
                           ip4_addr1_16(&pcb->remote_ip), ip4_addr2_16(&pcb->remote_ip),
@@ -1379,9 +1384,9 @@ tcp_keepalive(struct tcp_pcb *pcb)
                 ("tcp_keepalive: could not allocate memory for pbuf\n"));
     return;
   }
-  tcphdr = (struct tcp_hdr *)p->payload;
 
 #if CHECKSUM_GEN_TCP
+  tcphdr = (struct tcp_hdr *)p->payload;
   tcphdr->chksum = inet_chksum_pseudo(p, &pcb->local_ip, &pcb->remote_ip,
                                       IP_PROTO_TCP, p->tot_len);
 #endif
