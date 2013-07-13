34e0f6923c31a1407a3a73c5e86493a4509e3895
idiotcatalyst <idiotcatalyst@gmail.com>
2013-07-13 07:33:38 +0000

    Using an older version of the eth driver because otherwise a busy wait fails to init the ethernet driver correctly

--- code/system/libraries/STM32Libraries/STM32F4x7_ETH_Driver/inc/stm32f4x7_eth_conf_template.h
+++ code/system/libraries/STM32Libraries/STM32F4x7_ETH_Driver/inc/stm32f4x7_eth_conf_template.h
@@ -2,28 +2,22 @@
   ******************************************************************************
   * @file    stm32f4x7_eth_conf_template.h
   * @author  MCD Application Team
-  * @version V1.0.1
-  * @date    21-May-2012
+  * @version V1.0.0
+  * @date    14-October-2011
   * @brief   Configuration file for the STM32F407xx/417xx Ethernet driver.
   *          This file should be copied to the application folder and renamed to
   *          stm32f4x7_eth_conf.h    
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; COPYRIGHT 2012 STMicroelectronics</center></h2>
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
   *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
   ******************************************************************************
   */ 
 
@@ -94,5 +88,5 @@
 #endif /* __STM32F4x7_ETH_CONF_H */
 
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
 
